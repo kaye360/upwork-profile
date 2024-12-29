27dd187aca2f385d0f1835635e5720ad12d02126
joshkaye88 <jkaye@shaw.ca>
2024-12-28 22:31:33 -0700

    add some case studies

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -12,41 +12,41 @@ export const projects: Project[] = [
     {
         title : 'Tri-city Bikes and Brews',
         role : 'Web design + development',
-        tags : ['Wordpress blog', 'Custom theme', 'Modern'],
+        tags : ['Wordpress blog', 'Custom theme', 'Modern design'],
         cover : '/img/bikes-and-brews-cover.webp',
-        url : '/case-study/bikes-and-brews-homestay'
+        url : '/case-study/bikes-and-brews',
     },
 
     {
         title : 'Elite Canadian Homestay',
         role : 'Web design + development',
-        tags : ['Brochure website', 'Professional', 'Clean'],
+        tags : ['Brochure website', 'Professional design', 'Clean design'],
         cover : '/img/elite-homestay-cover.webp',
-        url : '/case-study/elite-canadian-homestay'
+        url : '/case-study/elite-homestay',
     },
 
     {
         title : 'Kelly Ann Creations',
         role : 'Web design + development',
-        tags : ['Brochure website', 'Minimalist', 'Modern'],
+        tags : ['Brochure website', 'Minimalist design', 'Modern design'],
         cover : '/img/kelly-ann-creations-cover.webp',
-        url : '/case-study/kelly-ann-creations'
+        url : '/case-study/kelly-ann-creations',
     },
 
     {
         title : 'Vitality Fitness',
         role : 'Web design + development',
-        tags : ['Wordpress', 'Modern', 'Custom theme'],
+        tags : ['Wordpress', 'Modern design', 'Custom theme'],
         cover : '/img/vitality-fitness-cover.webp',
-        url : '/case-study/vitality-fitness'
+        url : '/case-study/vitality-fitness',
     },
 
     {
         title : 'Megan Riley',
         role : 'Web design + development',
-        tags : ['Landing Page', 'Modern', 'Professional'],
+        tags : ['Landing Page', 'Modern design', 'Professional design'],
         cover : '/img/megan-riley-cover.webp',
-        url : '/case-study/megan-riley'
+        url : '/case-study/megan-riley',
     },
     
 ]
